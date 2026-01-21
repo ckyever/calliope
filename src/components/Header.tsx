0dc7cba89a54f4826462221c9a5f48f44753f065
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-21 18:41:21 +1100

    Add navbar to header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,16 @@ function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <img src={logo} alt="Calliope logo"></img>
-        <h1 className={styles["header-text"]}>Calliope</h1>
+        <div className={styles.logo}>
+          <img src={logo} alt="Calliope logo"></img>
+          <h1 className={styles["header-text"]}>Calliope</h1>
+        </div>
+        <nav className={styles.navbar}>
+          <ul className={styles["nav-links"]}>
+            <li>SIGN IN</li>
+            <li>CREATE ACCOUNT</li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
