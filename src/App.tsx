839e1c653d158f31587b9472da074336af6f07c5
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-26 18:39:53 +1100

    Store token received from auth callback in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { useSearchParams } from "react-router";
+
 import Header from "./components/Header";
 
 function App() {
+  const [searchParams] = useSearchParams();
+  const callbackToken = searchParams.get("token");
+
+  if (callbackToken) {
+    localStorage.setItem("token", callbackToken);
+  }
+
   return (
     <>
       <Header />
