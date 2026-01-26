95f464fd755bd4736ad1d697c419f80aa80bbdad
Ckyever Gaviola <ckyeverg@gmail.com>
2026-01-26 17:53:51 +1100

    Added error page

---
+++ src/components/ErrorPage.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+function ErrorPage() {
+  return (
+    <div>
+      <Header />
+      <h2>I think you might be lost</h2>
+    </div>
+  );
+}
+
+export default ErrorPage;
