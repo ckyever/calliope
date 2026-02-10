b0396a27309002372e100cdc5f14ba84b04be80f
Ckyever Gaviola <ckyeverg@gmail.com>
2026-02-10 15:08:53 +1100

    Show display name upon successful login

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
 
+import ENVIRONMENT_VARIABLES from "./environmentVariables";
 import Header from "./components/Header";
-
 import styles from "./styles/App.module.css";
 
 function App() {
+  const [displayName, setDisplayName] = useState("");
+
   const [searchParams] = useSearchParams();
   const callbackToken = searchParams.get("token");
 
-  if (callbackToken) {
+  useEffect(() => {
+    if (!callbackToken) return;
     localStorage.setItem("token", callbackToken);
-  }
+
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch(
+          `${ENVIRONMENT_VARIABLES.BACKEND_API_URL}/user`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${callbackToken}`,
+            },
+          },
+        );
+        const data = await response.json();
+        setDisplayName(data.user.displayName);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchProfile();
+  }, [callbackToken]);
 
   return (
     <>
       <Header />
-      <div className={styles["main-content"]}>Dashboard</div>
+      <div className={styles["main-content"]}>Welcome {displayName}</div>
     </>
   );
 }
