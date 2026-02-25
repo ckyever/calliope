604b2afd6166a3ffc87a2255c8a059fa69260346
Ckyever Gaviola <ckyeverg@gmail.com>
2026-02-25 22:03:04 +1100

    Add styling to sign in dialog

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 
 import ENVIRONMENT_VARIABLES from "../environmentVariables";
+import styles from "../styles/SignIn.module.css";
 
 function SignIn() {
   const [openModal, setOpenModal] = useState(false);
   return (
     <>
       <button onClick={() => setOpenModal(true)}>SIGN IN</button>
-      <dialog open={openModal}>
+      <dialog open={openModal} className={styles["sign-in-dialog"]}>
         <button onClick={() => setOpenModal(false)}>X</button>
         <a href={ENVIRONMENT_VARIABLES.AUTH_URL}>Continue with Spotify</a>
       </dialog>
