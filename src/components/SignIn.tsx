370502e53916cd4b7d5d64935d1ef339319fc33c
Ckyever Gaviola <ckyeverg@gmail.com>
2026-02-26 22:24:30 +1100

    Fix sign in dialog always being open

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import ENVIRONMENT_VARIABLES from "../environmentVariables";
 import styles from "../styles/SignIn.module.css";
 
 function SignIn() {
   const [openModal, setOpenModal] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    if (openModal) {
+      dialog.showModal();
+    } else {
+      dialog.close();
+    }
+  }, [openModal]);
+
   return (
     <>
       <button onClick={() => setOpenModal(true)}>SIGN IN</button>
-      <dialog open={openModal} className={styles["sign-in-dialog"]}>
-        <button onClick={() => setOpenModal(false)}>X</button>
-        <a href={ENVIRONMENT_VARIABLES.AUTH_URL}>Continue with Spotify</a>
+      <dialog
+        ref={dialogRef}
+        className={styles["sign-in-dialog"]}
+        onClose={() => setOpenModal(false)}
+      >
+        <div className={styles["sign-in-container"]}>
+          <button onClick={() => setOpenModal(false)}>X</button>
+          <a href={ENVIRONMENT_VARIABLES.AUTH_URL}>Continue with Spotify</a>
+        </div>
       </dialog>
     </>
   );
