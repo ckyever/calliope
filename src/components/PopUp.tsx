6f91051e171733702e188a8191d4fa03193dad73
Ckyever Gaviola <ckyeverg@gmail.com>
2026-03-05 13:56:25 +1100

    Refactor out the sign in dialog so it can be used for create account

---
+++ src/components/PopUp.tsx
@@ -0,0 +1,54 @@
+import type { ReactNode } from "react";
+import { useEffect, useRef, useState } from "react";
+
+import CloseButton from "./CloseButton";
+
+import styles from "../styles/PopUp.module.css";
+
+interface PopUpProps {
+  children: ReactNode;
+}
+
+function PopUp({ children }: PopUpProps) {
+  const [openModal, setOpenModal] = useState(false);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    const dialog = dialogRef.current;
+    if (!dialog) return;
+
+    if (openModal) {
+      dialog.showModal();
+    } else {
+      dialog.close();
+    }
+  }, [openModal]);
+
+  return (
+    <>
+      <button
+        className={styles["popup-button"]}
+        onClick={() => setOpenModal(true)}
+      >
+        SIGN IN
+      </button>
+      <dialog
+        ref={dialogRef}
+        className={styles["popup-dialog"]}
+        onClose={() => setOpenModal(false)}
+      >
+        <div className={styles["popup-container"]}>
+          <div className={styles["close-button"]}>
+            <CloseButton
+              onClick={() => setOpenModal(false)}
+              hexColour="#778899"
+            />
+          </div>
+          {children}
+        </div>
+      </dialog>
+    </>
+  );
+}
+
+export default PopUp;
--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,54 +1,17 @@
-import { useEffect, useRef, useState } from "react";
-
-import CloseButton from "../components/CloseButton";
-
 import ENVIRONMENT_VARIABLES from "../environmentVariables";
+import PopUp from "./PopUp";
 import styles from "../styles/SignIn.module.css";
 
 function SignIn() {
-  const [openModal, setOpenModal] = useState(false);
-  const dialogRef = useRef<HTMLDialogElement>(null);
-
-  useEffect(() => {
-    const dialog = dialogRef.current;
-    if (!dialog) return;
-
-    if (openModal) {
-      dialog.showModal();
-    } else {
-      dialog.close();
-    }
-  }, [openModal]);
-
   return (
-    <>
-      <button
-        className={styles["sign-in-button"]}
-        onClick={() => setOpenModal(true)}
+    <PopUp>
+      <a
+        className={styles["auth-button"]}
+        href={ENVIRONMENT_VARIABLES.AUTH_URL}
       >
-        SIGN IN
-      </button>
-      <dialog
-        ref={dialogRef}
-        className={styles["sign-in-dialog"]}
-        onClose={() => setOpenModal(false)}
-      >
-        <div className={styles["sign-in-container"]}>
-          <div className={styles["close-button"]}>
-            <CloseButton
-              onClick={() => setOpenModal(false)}
-              hexColour="#778899"
-            />
-          </div>
-          <a
-            className={styles["auth-button"]}
-            href={ENVIRONMENT_VARIABLES.AUTH_URL}
-          >
-            Continue with Spotify
-          </a>
-        </div>
-      </dialog>
-    </>
+        Continue with Spotify
+      </a>
+    </PopUp>
   );
 }
 
