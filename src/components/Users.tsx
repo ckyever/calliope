8ba5cc52727d82c8e6a6513b864b6838119b0f98
Ckyever Gaviola <ckyeverg@gmail.com>
2026-03-07 22:58:31 +1100

    Allow logged out users to view list of all users

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -8,21 +8,12 @@ import type { User as UserInfo } from "../types/User.ts";
 
 function Users() {
   const [users, setUsers] = useState<UserInfo[]>([]);
-  const { token } = useOutletContext<AppContext>();
 
   useEffect(() => {
     const fetchUsers = async () => {
-      if (!token) return;
       try {
         const response = await fetch(
           `${ENVIRONMENT_VARIABLES.BACKEND_API_URL}/users`,
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
-            },
-          },
         );
         const data = await response.json();
         setUsers(data.users);
@@ -31,7 +22,7 @@ function Users() {
       }
     };
     fetchUsers();
-  }, [token]);
+  }, []);
 
   return (
     <>
